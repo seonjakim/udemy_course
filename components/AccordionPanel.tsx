5a057552df024a6a11cbb2c932a60fb9cca94a0b
claire <seonja.kim0@gmail.com>
2022-04-17 19:54:26 +0900

    refactor: refactor AccordionPanel

--- components/AccordionPanel.tsx
+++ components/AccordionPanel.tsx
@@ -1,16 +1,51 @@
 import { useState } from "react";
 import { styled } from "@mui/material/styles";
-import ArrowForwardIosSharpIcon from "@mui/icons-material/ArrowForwardIosSharp";
 import MuiAccordion, { AccordionProps } from "@mui/material/Accordion";
 import MuiAccordionSummary, { AccordionSummaryProps } from "@mui/material/AccordionSummary";
 import MuiAccordionDetails from "@mui/material/AccordionDetails";
-import Typography from "@mui/material/Typography";
-import Image from "next/image";
-import { CourseContentType } from "@graphql/course/types";
+import { CourseContentLectureType, CourseContentType } from "@graphql/course/types";
+import LectureList from "./LectureList";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+
+type AccordionPanelProps = {
+  content: CourseContentType;
+};
+
+const AccordionPanel = ({ content }: AccordionPanelProps) => {
+  const [expanded, setExpanded] = useState<boolean>(false);
+  const {
+    name,
+    course_content_lectures,
+    course_content_lectures_aggregate: {
+      aggregate: {
+        count,
+        sum: { duration },
+      },
+    },
+  } = content;
+  return (
+    <div>
+      <Accordion expanded={expanded} onChange={() => setExpanded(!expanded)}>
+        <AccordionSummary aria-controls="panel1d-content" id="panel1d-header">
+          <div className="summary-content-container">
+            <h3>{name}</h3>
+            <span>{`${count}개 강의 • ${duration}`}</span>
+          </div>
+        </AccordionSummary>
+        <AccordionDetails>
+          {course_content_lectures.map((lecture: CourseContentLectureType) => (
+            <LectureList key={lecture.id} lecture={lecture} />
+          ))}
+        </AccordionDetails>
+      </Accordion>
+    </div>
+  );
+};
+
+export default AccordionPanel;
 
 const Accordion = styled((props: AccordionProps) => <MuiAccordion disableGutters elevation={0} square {...props} />)(
   ({ theme }) => ({
-    //#d1d7dc
     border: `1px solid ${theme.palette.divider}`,
     "&:not(:last-child)": {
       borderBottom: 0,
@@ -22,52 +57,30 @@ const Accordion = styled((props: AccordionProps) => <MuiAccordion disableGutters
 );
 
 const AccordionSummary = styled((props: AccordionSummaryProps) => (
-  <MuiAccordionSummary expandIcon={<ArrowForwardIosSharpIcon sx={{ fontSize: "0.7rem" }} />} {...props} />
+  <MuiAccordionSummary expandIcon={<KeyboardArrowDownIcon />} {...props} />
 ))(({ theme }) => ({
   backgroundColor: "#f7f9fa",
   flexDirection: "row-reverse",
   "& .MuiAccordionSummary-expandIconWrapper": {
-    transform: "rotate(-90deg)",
-    color: "#1c1d1f",
+    color: theme.palette.primary.contrastText,
   },
   "& .MuiAccordionSummary-expandIconWrapper.Mui-expanded": {
-    transform: "rotate(90deg)",
+    transform: "rotate(-180deg)",
   },
   "& .MuiAccordionSummary-content": {
     marginLeft: theme.spacing(1),
   },
+  "& .summary-content-container": {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+    width: "100%",
+    letterSpacing: "-.02rem",
+    lineHeight: "1.2",
+  },
 }));
 
 const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
-  padding: theme.spacing(2),
+  padding: theme.spacing(2, 3),
   borderTop: "1px solid rgba(0, 0, 0, .125)",
 }));
-
-type CustomizedAccordionsProps = {
-  content: CourseContentType;
-};
-
-export default function CustomizedAccordions({ content }: CustomizedAccordionsProps) {
-  const [expanded, setExpanded] = useState<boolean>(false);
-
-  return (
-    <div>
-      <Accordion expanded={expanded} onChange={() => setExpanded(!expanded)}>
-        <AccordionSummary aria-controls="panel1d-content" id="panel1d-header">
-          <Typography sx={{ fontSize: "16px", fontWeight: "700", letterSpacing: "-.02rem", lineHeight: "1.2" }}>
-            {content.name}
-          </Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          {content.course_content_lectures.map(lecture => (
-            <Typography key={lecture.id}>
-              <Image src={"/favicon.ico"} width="8" height="8" alt="play-icon" />
-              {lecture.name}
-              {lecture.duration}
-            </Typography>
-          ))}
-        </AccordionDetails>
-      </Accordion>
-    </div>
-  );
-}
