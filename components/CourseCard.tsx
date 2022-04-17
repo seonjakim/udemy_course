4539f84fd08f08d8c587471fb27f7a77a12c830d
claire <seonja.kim0@gmail.com>
2022-04-17 19:54:52 +0900

    refactor: refactor CourseCard

--- components/CourseCard.tsx
+++ components/CourseCard.tsx
@@ -1,31 +1,83 @@
-import { Box } from "@mui/material";
+import { CourseList } from "@graphql/courses/types";
+import { styled } from "@mui/material/styles";
 import Image from "next/image";
+import Link from "next/link";
+import Rating from "./Rating";
 
-const CourseCard = ({ el }) => {
-  const { cover_url, name, price, short_description, instructors, feedbacks_aggregate } = el;
+type CourseCardProps = {
+  course: CourseList;
+};
+
+const CourseCard = ({ course }: CourseCardProps) => {
+  const {
+    cover_url,
+    name,
+    price,
+    short_description,
+    instructors,
+    feedbacks_aggregate: {
+      aggregate: {
+        avg: { rating },
+      },
+    },
+    id,
+  } = course;
   return (
-    <Box
-      sx={{
-        borderBottom: "1px solid #d1d7dc",
-        padding: "1rem 0",
-        height: "11rem",
-        display: "grid",
-        gridTemplateColumns: "auto 1fr",
-      }}
-    >
-      <div>
-        <img style={{ width: "125px" }} src={cover_url} alt="test" />
-      </div>
-      <div>
-        <h4>{name}</h4>
-        {instructors.map(instructor => (
-          <span key={instructor.id}>{instructor.name}</span>
-        ))}
-        <div>{feedbacks_aggregate.aggregate.avg.rating}</div>
-        <div>${price}</div>
-      </div>
-    </Box>
+    <Link href={`/${id}`} passHref>
+      <Container>
+        <div>
+          <Image width={260} height={145} src={cover_url} alt="course-cover-image" />
+        </div>
+        <div className="course-info-container">
+          <h3>{name}</h3>
+          <p>{short_description}</p>
+          <div className="instructor-container">
+            {instructors.map(instructor => (
+              <span key={instructor.id}>{instructor.name}</span>
+            ))}
+          </div>
+          <div className="rating-container">
+            <strong>{rating}</strong>
+            <Rating value={rating} readOnly />
+          </div>
+        </div>
+        <div>
+          <strong style={{ fontSize: "16px" }}>${price}</strong>
+        </div>
+      </Container>
+    </Link>
   );
 };
+
+const Container = styled("div")`
+  padding: ${({ theme }) => theme.spacing(2, 0)};
+  background-color: #fff;
+  display: grid;
+  grid-template-columns: auto 1fr auto;
+  gap: ${({ theme }) => theme.spacing(2)};
+  & > .course-info-container {
+    display: flex;
+    flex-direction: column;
+    gap: 4px;
+    & > .instructor-container {
+      overflow: hidden;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+      color: ${({ theme }) => theme.palette.secondary.contrastText};
+      font-size: 12px;
+    }
+  }
+  & > div .rating-container {
+    display: flex;
+    gap: 4px;
+    align-items: center;
+    color: #b4690e;
+    line-height: 1.2;
+    & > label,
+    span {
+      font-size: 14px;
+    }
+  }
+`;
 
 export default CourseCard;
